245fb6d5f14068691956e9a97b06aee151655f7e
N-drue <n-drue@yandex.ru>
2016-11-23 23:00:37 +0300

    Готово

--- js/js.js
+++ js/js.js
@@ -1,40 +1,41 @@
-﻿var link = document.querySelector(".btn-map");
-var popup = document.querySelector(".modal-content");
-link.addEventListener("click", function(event) {
-	event.preventDefault();
-	popup.classList.add("modal-content-show");
-});
+//обратная связь
+var link = document.querySelector(".btn-contacts");
+  var popup = document.querySelector(".modal-content");
+  var close = popup.querySelector(".modal-content-close");
+  var form = popup.querySelector("form");
+  var login = popup.querySelector("[name=login]");
+  var email = popup.querySelector("[name=email]");
+  var storage = localStorage.getItem("login");
 
-window.addEventListener("keydown", function(event) {
-	if(event.keyCode === 27) {
-		if(popup.classList.contains("modal-content-show")) {
-			popup.classList.remove("modal-content-show");
-		}
-	}
-});
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("modal-content-show");
+    if (storage) {
+      login.value = storage;
+      email.focus();
+    } else {
+      login.focus();
+    }
+  });
 
+  close.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
+  });
 
-ymaps.ready(init);
-var myMap,
-	myPlacemark;
-	YMaps.Style;
-function init () {
-	var myMap = new ymaps.Map("map", {
-		center: [55.76, 37.64],
-		zoom: 18,
-		controls: []
-	});
-			myPlacemark = new ymaps.Placemark([55.76, 37.64], {
-				iconContent: "1",
-				hintContent: "Nerd",
-				balloonContent: "191186, Санкт-Петербург, ул. Б. Конюшенная, 19/8",
-		},{
-						iconLayout: "default#image",
-						iconImageHref: "../img/map-marker.png",
-						iconImageSize: [376, 190],
-						iconImageOffset: [-13, -70]
-				});
+  form.addEventListener("submit", function(event) {
+    if (!login.value || !email.value) {
+      event.preventDefault();
+      console.log("Нужно ввести логин и e-mail");
+    } else {
+      localStorage.setItem("login", login.value);
+    }
+  });
 
-		myMap.geoObjects.add(myPlacemark);
-		myMap.behaviors.disable("scrollZoom");
-}
+    window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      if (popup.classList.contains("modal-content-show")) {
+        popup.classList.remove("modal-content-show");
+      }
+    }
+  });
